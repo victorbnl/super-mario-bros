01129008a1ada5353b3bc1528616e904720e32b5
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-12-14 22:20:25 +0100

    Improve camera

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <SDL2/SDL.h>
 
 #include "camera.h"
@@ -21,18 +22,19 @@ void Camera::update()
     int charOnScreenPosX = mCharacter->pos.x - x;
 
     // If character is too much at the left
-    if (charOnScreenPosX < 150)
+    if (charOnScreenPosX < SCREEN_PADDING_X)
     {
         // If camera is after the beginning of the level
         if (x >= 0)
         {
-            x -= 150 - charOnScreenPosX;
+            // std::max ensures camera doesn't go to the left of the level
+            x = std::max(x - (SCREEN_PADDING_X - charOnScreenPosX), 0);
         }
     }
 
     // If character is too much at the right
-    if (charOnScreenPosX > SCREEN_WIDTH - 150)
+    if (charOnScreenPosX > SCREEN_WIDTH - SCREEN_PADDING_X)
     {
-        x += (mCharacter->pos.x) - (x + SCREEN_WIDTH - 150);
+        x += (mCharacter->pos.x) - (x + SCREEN_WIDTH - SCREEN_PADDING_X);
     }
 }
--- src/constants.h
+++ src/constants.h
@@ -7,9 +7,10 @@ const int TILE_SIZE = 48;
 // Window size
 const int SCREEN_WIDTH = 15 * TILE_SIZE;
 const int SCREEN_HEIGHT = 9 * TILE_SIZE;
+const int SCREEN_PADDING_X = 150;
 
 // Speed
-const int SPEED = 5;
+const int SPEED = 10;
 const float GRAVITY = 0.5;
 
 #endif
