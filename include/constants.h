350eecc984a42c2cbf7844595a85cfbc313909a7
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-12-19 10:41:17 +0100

    Use inline constants

--- include/constants.h
+++ include/constants.h
@@ -2,17 +2,17 @@
 #define CONSTANTS_H
 
 // Tile size
-const int TILE_SIZE = 48;
+inline const int TILE_SIZE = 48;
 
 // Window size
-const int SCREEN_WIDTH = 15 * TILE_SIZE;
-const int SCREEN_HEIGHT = 9 * TILE_SIZE;
-const int SCREEN_PADDING_X = 250;
+inline const int SCREEN_WIDTH = 15 * TILE_SIZE;
+inline const int SCREEN_HEIGHT = 9 * TILE_SIZE;
+inline const int SCREEN_PADDING_X = 250;
 
 // Speed
-const int CHARACTER_SPEED = 6;
-const float GRAVITY = 20;
-const float GRAVITY_STEPS = 0.2;
-const float JUMP_HEIGHT = GRAVITY * 0.4;
+inline const int CHARACTER_SPEED = 6;
+inline const float GRAVITY = 20;
+inline const float GRAVITY_STEPS = 0.2;
+inline const float JUMP_HEIGHT = GRAVITY * 0.4;
 
 #endif
